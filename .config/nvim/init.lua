156eede59503b23d7781eb635d36cce7ff611d1e
Joanne Santhosh <joanne.santhosh@mths.ca>
2024-03-17 17:35:59 +0000

    added lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,17 +1,18 @@
-vim.cmd("set encoding=utf-8")
-vim.cmd("set termguicolors")
+require "user.options"
+require "user.keymaps"
 
-vim.cmd("set number")
-vim.cmd(set relativenumber")
-vim.cmd("set cursorline")
-vim.cmd("set numberwidth=4")
-vim.cmd("set tabstop=2")↵
-vim.cmd("set expandtab")
-vim.cmd("set smartindent")
-vim.cmd("set colorcolumn=80")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd("set scrolloff=6")
-vim.cmd("set wrap!")
 
-vim.cmd("set listchars=space:·,eol:↵ ")
-vim.cmd("set list")
+require("lazy").setup(plugins, opts)
