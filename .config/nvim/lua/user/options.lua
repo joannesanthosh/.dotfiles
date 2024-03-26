995a6ef25733d594163c2cfc1cc16b9c0a4ea8a0
Mr Coxall <mr.coxall@mths.ca>
2024-03-26 13:16:16 +0000

    merge

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -2,7 +2,7 @@ vim.cmd("set·encoding=utf-8")↵
 vim.cmd("set·termguicolors")↵                                                                                                                              
 
 vim.cmd("set·number")↵                                                                                                                          
-vim.cmd(set·relativenumber")↵                                                                                                                   
+vim.cmd("set·relativenumber")↵                                                                                                                   
 vim.cmd("set·cursorline")↵                                                                                                                      
 vim.cmd("set·numberwidth=4")↵                                                                                                                   
 vim.cmd("set·tabstop=2")↵↵                                                                                                                      
